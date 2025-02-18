f1afdef12eff35fc49f78d3d1e052b16cb1e31ec
Sidra <sidrasajjad0987@gmail.com>
2025-02-18 22:16:22 +0500

    Add files via upload

---
+++ check if the number is divisible by 5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"enter the number"<<endl;
+	cin>>num;
+	if(num%5==0){
+		cout<<"The number is divisible by 5"<<endl;
+	}
+	else{
+		cout<<"The number is not divisible by 5"<<endl;
+	}
+}
